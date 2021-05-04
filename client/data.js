ca1c564da3459130ae0373999ae12113d6699bad
CRKarur <chandrikarathnakarur@gmail.com>
2021-05-03 20:05:28 -0400

    angular components and main navigation added

---
+++ client/data.js
@@ -0,0 +1,33 @@
+module.exports = function () {
+    return {
+        books:
+            [
+                {
+                    "_id": 1,
+                    "name": "Grand Mother's Stories",
+                    "author": "Sudha Murthy",
+                    "Published": 2015,
+                    "description": "grandmother narrates think tales to her grand children who are at her home for summer",
+                    "price": 5.23
+                },
+                {
+                    "_id": 2,
+                    "name": "Grand Mother's Stories 2",
+                    "author": "Sudha Murthy",
+                    "Published": 2016,
+                    "description": "grandmother narrates think tales to her grand children who are at her home for summer",
+                    "price": 6.23
+                },
+                {
+                    "_id": 3,
+                    "name": "Grand Mother's Stories",
+                    "author": "Sudha Murthy",
+                    "Published": 2017,
+                    "description": "grandmother narrates think tales to her grand children who are at her home for summer",
+                    "price": 7.23
+                }
+
+            ],
+            orders: []
+    };
+};
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './partials/header/header.component';
+import { FooterComponent } from './partials/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { BasePageComponent } from './partials/base-page/base-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ProductsComponent,
+    ServicesComponent,
+    BasePageComponent
   ],
   imports: [
     BrowserModule,
